e7f43ba576e1d713845713143d9707e856805541
yurure32 <yurure32@gmail.com>
2018-10-26 07:01:50 +0000

    user login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,17 @@
 @section('content')
     <div class="center jumbotron">
         <div class="text-center">
-            <h1>Welcome to the Blog.</h1>
-            {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            @if (Auth::check())
+                <?php $user = Auth::user(); ?>
+                {{ $user->name }}
+            @else
+                <div class="center jumbotron">
+                    <div class="text-center">
+                        <h1>Welcome to the Blog.</h1>
+                        {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 @endsection
